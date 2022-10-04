e8163bd4c79a6a57b52951d3c34aa7b61400e4cf
Francis <francisodhiambo2@gmail.com>
2022-10-04 10:31:14 +0300

    commit tree 0a355826060f16466399f9ec48ad1e0bab50db80

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,3 +1,30 @@
+# class PlantsController < ApplicationController
+
+#   # GET /plants
+#   def index
+#     plants = Plant.all
+#     render json: plants
+#   end
+
+#   # GET /plants/:id
+#   def show
+#     plant = Plant.find_by(id: params[:id])
+#     render json: plant
+#   end
+
+#   # POST /plants
+#   def create
+#     plant = Plant.create(plant_params)
+#     render json: plant, status: :created
+#   end
+
+#   private
+
+#   def plant_params
+#     params.permit(:name, :image, :price, :is_in_stock)
+#   end
+# end
+
 class PlantsController < ApplicationController
 
   # GET /plants
@@ -16,6 +43,20 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  # UPDATE /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
   end
 
   private
